5835aa2586ade9142a065f005020a35810c3262a
tiendainguyen <daint.dev@gmail.com>
2023-04-11 20:31:20 +0700

    create

---
+++ src/main/java/com/example/redisservice/utils/MapperUtil.java
@@ -0,0 +1,10 @@
+package com.example.redisservice.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapperUtil {
+  public static Map<String, Object> toMap(Object obj) {
+    return new HashMap<>();
+  }
+}
